4243fc86cd678b4a3643d4841c0947e7b25010d7
DALGA Ozguc <ozgucdalga@gmail.com>
2022-03-21 01:14:48 -0400

    web Token integrtion in express server

--- api/index.js
+++ api/index.js
@@ -1,7 +1,53 @@
 const express = require('express');
 const app = express();
+const jwt = require('jsonwebtoken')
+app.use(express.json());
 
 
-app.listen(5000, ()=>{ 
+// fake usedRules  server memories
+const users = [
+    {
+        id: "1",
+        name: "John",
+        password: "John0908",
+        isAdmin: true,
+    }, {
+        id: "2",
+        name: "Jane",
+        password: "Jane0908",
+        
+    }
+];
+
+
+app.post("/api/login", (req, res) => {
+
+   const {username,password} =req.body;
+   const user = users.find((u) =>{
+       return u.username ===username && u.password ===password;
+   });
+
+   if(user){
+       // generate an access token for
+       const accessToken =jwt.sign(
+           {id:user.id,
+            isAdmin:user.isAdmin,
+        },
+        "mySecretKey"
+       );
+       res.json({
+           username:user.username,
+           isAdmin:user.isAdmin,
+           accessToken,
+       })
+   }else{
+       res.status(400).json("username or password incorrect")
+   }
+
+   
+});
+
+
+app.listen(5000, () => {
     console.log(`Backend server is running now`)
 }) 
